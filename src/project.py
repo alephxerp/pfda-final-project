983dcfe757f5ebc95a8f23aedbbb7024a020ffbb
alephxerp <alephxerpanims@gmail.com>
2026-05-02 15:06:36 -0500

    feat: create base game loop

--- src/project.py
+++ src/project.py
@@ -1,8 +1,29 @@
-import pygame
+import pygame, sys
 
 
 def main():
-    pass
+    pygame.init()
+    resolution : tuple[int, int] = (960, 540)
+    running : bool = True
+
+    pygame.display.set_caption("Final Project")
+    screen : pygame.display = pygame.display.set_mode(resolution, pygame.SHOWN)
+
+    clock = pygame.time.Clock()
+    fps = 60
+    delta = 0
+
+    while running:
+        screen.fill((0, 0, 0))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        pygame.display.flip()
+        delta = clock.tick(fps)
+        
+    sys.exit()
 
 
 if __name__ == "__main__":
