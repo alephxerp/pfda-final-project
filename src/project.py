7929b1537a689b8eda7b0fed4d03b1dc0cb015dd
Andrew Yu <alephxerpanims@gmail.com>
2026-05-03 19:19:48 -0500

    feat: move rendering to separate surface
    allows for rescaling of content along with window size.

--- src/project.py
+++ src/project.py
@@ -1,25 +1,41 @@
 import pygame, sys
 
 
+class Mouse():
+    def __init__(self):
+        self.position = [0, 0]
+
+    def update():
+        pass
+
+
+class Button():
+    def __init__(self):
+        self.position : tuple[int, int] = (0, 0)
+
+
+
 def main():
     pygame.init()
-    resolution : tuple[int, int] = (960, 540)
+    resolution : tuple[int, int] = (1280, 720)
     running : bool = True
 
     pygame.display.set_caption("Final Project")
-    screen : pygame.display = pygame.display.set_mode(resolution, pygame.SHOWN)
+    screen : pygame.Surface = pygame.display.set_mode(resolution, pygame.RESIZABLE)
+    viewport : pygame.Surface = screen.copy()
 
     clock = pygame.time.Clock()
     fps = 60
     delta = 0
 
     while running:
-        screen.fill((0, 0, 0))
+        viewport.fill((0, 0, 0))
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
+        screen.blit(pygame.transform.scale(viewport, screen.get_rect().size), (0, 0))
         pygame.display.flip()
         delta = clock.tick(fps)
         
