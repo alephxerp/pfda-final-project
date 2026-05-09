757baee0546dc4db9e784f3a9daaa2fe856e55cc
Andrew Yu <alephxerpanims@gmail.com>
2026-05-09 16:45:49 -0500

    fix: button properly calls function on arguments

--- src/button.py
+++ src/button.py
@@ -2,11 +2,12 @@ import pygame, os
 from element import Element
 
 class Button(Element):
-    def __init__(self, sprite : str, function : callable, position : tuple[int, int] = (0, 0)) -> None:
+    def __init__(self, sprite : str, function : callable, *args, position : tuple[int, int] = (0, 0)) -> None:
         Element.__init__(self, sprite, position)
 
         self.function : callable = function
+        self.args : tuple = args
 
     
-    def on_click(self) -> None:
-        self.function()
+    def on_click(self):
+        return self.function(*self.args)
