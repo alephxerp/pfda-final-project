ec0cf6939eee05191bdd790aa655dc6663e4f7b5
Andrew Yu <alephxerpanims@gmail.com>
2026-05-10 00:12:44 -0500

    feat: build character and character groups

--- src/character.py
+++ src/character.py
@@ -1,5 +1,25 @@
-import pygame
+import pygame, os
+from element import Element, ElementGroup
 
-class Character:
-    pass
+class Character(Element):
+    def __init__(self, sprite : str, dialogue : str, position : tuple[int, int] = (0, 0)) -> None:
+        Element.__init__(self, sprite, position)
+
+        self.dialogue : str = dialogue
+
+
+    def on_click(self) -> str:
+        return self.dialogue
+
+
+class CharacterGroup(ElementGroup):
+    def __init__(self) -> None:
+        ElementGroup.__init__(self)
+    
+
+    def create(self, sprite : str, dialogue: str, position : tuple[int, int] = (0, 0)) -> Element:
+        element : Character = Character(sprite, dialogue, position)
+        self.add(element)
+
+        return element
 
